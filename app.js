9f6d65695de9666f32d89e0ceabb39c2fa86dcb0
Sean Heffernan <s.heffernan1@nuigalway.ie>
2013-09-09 13:26:21 +0100

    Added auth system

--- app.js
+++ app.js
@@ -4,10 +4,63 @@
  */
 
 var express = require('express')
+  , flash = require('connect-flash')
+  , passport = require('passport')
   , routes = require('./routes')
   , torrents = require('./routes/torrentrestapi')
+  , auth = require('./routes/auth')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , LocalStrategy = require('passport-local').Strategy;
+
+var users = [
+    { id:1, username: 'sean', password: 'spades' }
+];
+
+function findById(id, fn) {
+    var idx = id - 1;
+    if (users[idx]) {
+        fn(null, users[idx]);
+    } else {
+        fn(new Error('User ' + id + ' does not exist'));
+    }
+}
+
+function findByUsername(username, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.username === username) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+}
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+        findByUsername(username, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: 'Unknown user ' + username });
+            }
+            if (user.password != password) {
+                return done(null, false, { message: 'Invalid password' });
+            }
+            return done(null, user);
+        })
+    }
+));
 
 var app = express();
 
@@ -16,6 +69,11 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'omglol' }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -27,9 +85,27 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/torrents', torrents.get);
+app.get('/', ensureAuthenticated, function(req, res) {
+    routes.index(req, res), { user: req.user }
+});
+app.get('/torrents', ensureAuthenticated, torrents.get);
+app.get('/login', auth.login);
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function(req, res) {
+    res.redirect('/');
+});
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login')
+}
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+/*
+ * GET login page.
+ */
+
+
+exports.login = function (req, res) {
+    res.render('login');
+};
+
