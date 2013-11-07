8a87da6689678c07fb154c9a4a756e1cc4cece0e
Sean Heffernan <s.heffernan1@nuigalway.ie>
2013-11-07 16:15:50 +0000

    Added mobile meta tag

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ passport.use(new LocalStrategy(function(username, password, done) {
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
