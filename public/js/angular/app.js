e3ff7128d0b9d4ec49078317a59a883210e914c1
Sean Heffernan <s.heffernan1@nuigalway.ie>
2014-01-27 12:59:06 +0000

    Split angular code out into various modules

---
+++ public/js/angular/app.js
@@ -0,0 +1,28 @@
+/**
+ * Define our angular app and its dependencies
+ */
+var app = angular.module('myApp', [
+    'ui.bootstrap',
+    'ngRoute',
+    'myApp.filters',
+    'myApp.directives',
+    'myApp.controllers'
+]);
+
+
+/**
+ * Define our angular routes - i.e. what content is rendered depending on the URL
+ */
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/main/:torrentHash', {
+            templateUrl: 'partials/detailed_info',
+            controller: 'DetailedInfoCtrl'
+        }).
+        when('/main', {
+            templateUrl: 'partials/torrents'
+        }).
+        otherwise({
+            redirectTo: '/main'
+        });
+}]);
---
+++ public/js/angular/filters.js
@@ -0,0 +1,45 @@
+/**
+ * This module contains our angular filters
+ * We define the module and inject its dependencies
+ */
+var app = angular.module('myApp.filters', [])
+
+
+/**
+ * An Angular filter function to determine if a torrent is seeding
+ * @param torrent The torrent to evaluate
+ * @returns {boolean} Returns a boolean to denote if the torrent is seeding
+ */
+app.seedingFilter = function (torrent) {
+    return torrent.complete == 1
+};
+
+
+/**
+ * An Angular filter function to determine if a torrent is leeching
+ * @param torrent The torrent to evaluate
+ * @returns {boolean} Returns a boolean to denote if the torrent is leeching
+ */
+app.leechingFilter = function (torrent) {
+    return torrent.complete == 0
+};
+
+
+/**
+ * An Angular filter function to determine if a torrent is currently currently uploading
+ * @param torrent The torrent to evaluate
+ * @returns {boolean} Returns a boolean to denote if the torrent is currently uploading
+ */
+app.uploadingFilter = function (torrent) {
+    return torrent.uploadRate > 0
+};
+
+
+/**
+ * An Angular filter function to determine if a torrent is currently currently downloading
+ * @param torrent The torrent to evaluate
+ * @returns {boolean} Returns a boolean to denote if the torrent is currently downloading
+ */
+app.downloadingFilter = function (torrent) {
+    return torrent.downloadRate > 0
+};
