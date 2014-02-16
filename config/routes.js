229d281b6f9cb1b989b0cc38ec9d2fd27a6b838d
Sean Heffernan <s.heffernan1@nuigalway.ie>
2014-02-16 13:02:40 +0000

    Tidied up code - moved routes to separate file and added some comments

---
+++ config/routes.js
@@ -0,0 +1,61 @@
+var routes = require('../routes/general');
+var torrents = require('../routes/torrentrestapi');
+var system = require('../routes/systemapi');
+
+
+/**
+ * This function defines the routes in our application
+ * @param app The express app
+ * @param passport The passport authentication middleware
+ */
+module.exports = function(app, passport) {
+
+    /**
+     * View routes - routes that render or deal with views
+     */
+    app.get('/', ensureAuthenticated, routes.index);
+    app.get('/login', routes.login);
+    app.post('/login', passport.authenticate('local', { failureRedirect: '/login'}), function(req, res) {
+        res.redirect('/');
+    });
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
+    app.get('/partials/:name', ensureAuthenticated, function (req, res) {
+        var name = req.params.name;
+        res.render('../public/partials/' + name);
+    });
+    app.post('/settings', ensureAuthenticated, system.setSettings);
+
+
+    /**
+     * RESTful services routes - routes that define RESTful API's
+     */
+    app.post('/add-torrent', ensureAuthenticated, system.addTorrent);
+    app.post('/add-torrent-url', ensureAuthenticated, system.addTorrentURL);
+    app.get('/torrents', ensureAuthenticated, torrents.getTorrents);
+    app.get('/files/:hash', ensureAuthenticated, torrents.getFileInfo);
+    app.get('/trackers/:hash', ensureAuthenticated, torrents.getTrackerInfo);
+    app.get('/info/:hash', ensureAuthenticated, torrents.getDetailedTorrentInfo);
+    app.get('/peers/:hash', ensureAuthenticated, torrents.getPeerInfo);
+    app.get('/stop/:hash', ensureAuthenticated, torrents.stopTorrent);
+    app.get('/start/:hash', ensureAuthenticated, torrents.startTorrent);
+    app.get('/stats', ensureAuthenticated, torrents.getStats);
+    app.get('/settings', ensureAuthenticated, system.getSettings);
+    app.get('/system-stats', ensureAuthenticated, system.getSystemStats);
+
+
+    /**
+     * A function to ensure users are authenticated to view specific routes
+     * @param req The HTTP request
+     * @param res The HTTP response
+     * @param next The 'next' callback to run if the user is authenticated
+     */
+    function ensureAuthenticated(req, res, next) {
+        if ('development' == app.get('env') || req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('/login')
+    }
+}
