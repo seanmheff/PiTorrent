188cf92873fef7495b42848e830ea1f5e1bd149a
Sean Heffernan <s.heffernan1@nuigalway.ie>
2014-05-08 21:51:29 +0100

    Updated installer

--- installer.py
+++ installer.py
@@ -7,16 +7,65 @@ import sys
 """
 # Hepler function to validate a return code - exits if non 0
 """
-def validateReturnCode(rc):
+def validateReturnCode(rc, exit=True):
 	if rc != 0:
 		print "Command exited with rc: " + str(rc)
-		sys.exit(1)
+		if exit:
+			sys.exit(1)
 	else:
-		print "OK" 
+		print "OK\n" 
 
 
 """
-# Helper function to replace variables in the daemon shell script
+# Function to check for the presence of a specific program on the system
+"""
+def checkForProgram(programName):
+	p = Popen(["whereis", programName], stdout=PIPE)
+	output, err = p.communicate()
+	
+	if err != None:
+		print "Error when checking for " + programName 
+		print err
+		sys.exit(1)
+	else:
+		# Output should look like: "node: /usr/local/bin/node"
+		# If node is not found, output should look like: "node: "
+		tmp = output.split(" ")
+		if len(tmp) > 1:
+			return tmp[1][:-1] # return second string in list & remove trailing newline character
+		else:
+			print programName + " is not found on this system..."
+			return None
+
+
+"""
+# Install rTorrent via APT
+"""
+def installRTorrent():
+	print("Checking to see if rTorrent is previously installed...")
+	if checkForProgram("rtorrent") == None:
+		print("rTorrent not found, installing...")
+		rc = call(["sudo", "apt-get", "install",  "rTorrent"])
+		validateReturnCode(rc)
+	else:
+		print("rTorrent already installed\n")
+
+
+"""
+# Install Screen via APT
+"""
+def installScreen():
+	print("Checking to see if Screen is previously installed...")
+	if checkForProgram("rtorrent") == None:
+		print("Screen not found, installing...")
+		rc = call(["sudo", "apt-get", "install",  "creen"])
+		validateReturnCode(rc)
+	else:
+		print("Screen already installed\n")
+
+
+"""
+# Helper function to replace variables in a daemon shell script
 """
 def replaceDaemonVariables(varsDict, daemonFileLocation):
 	for line in fileinput.input(daemonFileLocation, inplace=True):
@@ -51,8 +100,8 @@ def installRtorrentDaemon(user, daemonFileLocation):
 	print "OK"
 	
 	# Move the daemon to its rightful place
-	print "Moving script to /etc/init.d/ - need root access: "
-	rc = call(["sudo", "cp", daemonFileLocation,  "/etc/"])
+	print "Moving script to /etc/init.d/"
+	rc = call(["sudo", "cp", daemonFileLocation,  "/etc/init.d/"])
 	validateReturnCode(rc)
 
 	# Update the init daemon
@@ -60,30 +109,7 @@ def installRtorrentDaemon(user, daemonFileLocation):
 	call(["sudo", "update-rc.d", "rtorrent", "defaults"])
 	validateReturnCode(rc)
 
-	print "rTorrent daemon sucessfully installed"
-
-
-"""
-# Function to check for node.js on the system
-"""
-def checkForNodeJS():
-	p = Popen(["whereis", "node"], stdout=PIPE)
-	output, err = p.communicate()
-	
-	if err != None:
-		print "Error when checking for node.js: " 
-		print err
-		sys.exit(1)
-	else:
-		# Output should look like: "node: /usr/local/bin/node"
-		# If node is not found, output should look like: "node: "
-		tmp = output.split(" ")
-		if len(tmp) > 1:
-			return tmp[1]
-		else:
-			print "Node.js is not found on this system... exiting"
-			sys.exit(1)
-
+	print "rTorrent daemon sucessfully installed\n"
 
 
 """
@@ -98,10 +124,10 @@ def getSecondLastOccuranceOfChar(string, char):
 """
 def installPiTorrentDaemon(user, daemonFileLocation):
 	# Find our node binary directory
-	nodeBinDir = checkForNodeJS()
+	nodeBinDir = checkForProgram("node")
 	
 	# Find our node global modules directoty - can be derived from 'nodeBinDir'
-	nodeGlobalModulesDir = nodeBinDir[:getSecondLastOccuranceOfChar(nodeBinDir, "/")] + "/usr/local"
+	nodeGlobalModulesDir = nodeBinDir[:getSecondLastOccuranceOfChar(nodeBinDir, "/")] + "/lib/node_modules"
 	
 	appDir = os.getcwd()
 	appName = "app.js"
@@ -109,7 +135,7 @@ def installPiTorrentDaemon(user, daemonFileLocation):
 	logFile = appDir + "/PiTorrent.log"
 
 	# Create a dict of variables
-	varsDict = {"nodeBinDir":nodeBinDir, "nodeGlobalModulesDir":nodeGlobalModulesDir, "appDir":appDir, "appName":appName, "pidFile":pidFile, "logFile":logFile}
+	varsDict = {"nodeBinDir":nodeBinDir, "nodeGlobalModulesDir":nodeGlobalModulesDir, "appDir":appDir, "appName":appName, "pidFile":pidFile, "logFile":logFile, "user":user}
 
 	print "Installing PiTorrent daemon as user: " + user
 	print "Replacing script variables... ",
@@ -118,7 +144,7 @@ def installPiTorrentDaemon(user, daemonFileLocation):
 
 	# Move the daemon to its rightful place
 	print "Moving script to /etc/init.d/ - need root access: "
-	rc = call(["sudo", "cp", daemonFileLocation,  "/etc/"])
+	rc = call(["sudo", "cp", daemonFileLocation,  "/etc/init.d/"])
 	validateReturnCode(rc)
 
 	# Update the init daemon
@@ -126,11 +152,70 @@ def installPiTorrentDaemon(user, daemonFileLocation):
 	call(["sudo", "update-rc.d", "pitorrent", "defaults"])
 	validateReturnCode(rc)
 
-	print "PiTorrent daemon sucessfully installed"
+	print "PiTorrent daemon sucessfully installed\n"
+
+
+"""
+# Configure rTorrent config file
+"""
+def configRtorrent(user, configFile):
+	# Create some new directories that PiTorrent needs
+	downDir = "/home/" + user + "/PiTorrent.Downloads"
+	print "Creating download directory: " + downDir 
+	validateReturnCode(call(["mkdir", downDir]), exit=False)
+
+	sessionDir = "/home/" + user + "/.PiTorrent.session"
+	print "Creating session direstory: " + sessionDir
+	validateReturnCode(call(["mkdir", sessionDir]), exit=False)
+
+	torrentDir = "/home/" + user + "/PiTorrent.Torrents"
+	print "Creating torrent file directory: " + torrentDir
+	validateReturnCode(call(["mkdir", torrentDir]), exit=False)
+
+	# Append data to config
+	with open(configFile, "a") as f:
+		# Create PiTorrent download dir and add it to config
+		f.write("\n# Default directory to save the downloaded torrents.\n")
+		f.write("directory = /home/" + user + "/PiTorrent.Downloads\n")
+    	
+    	# Create rTorrent session dir
+		f.write("\n# Default session directory. Do NOT change or rTorrent daemon will fail!\n")
+		f.write("session = /home/" + user + "/.PiTorrent.session\n")
+
+    	# Create PiTorrent torrent dir and add it to config
+		f.write("\n# Default directory to save the downloaded torrents.\n")
+		f.write("schedule = watch_directory,5,5,load_start=/home/" + user + "/PiTorrent.Torrents/*.torrent\n")
+    	
+
+
+"""
+# Move the rTorrent config file to the correct dir
+"""
+def moveRtorrentConfig(user, configFile):
+	print "Moving rTorrent config file to /home/" + user + "/.rtorrent.rc ..."
+	rc = call(["sudo", "cp", configFile, "/home/" + user + "/.rtorrent.rc"])
+	validateReturnCode(rc)    	
+
+
+"""
+# Function to install PiTorrent node dependencies - i.e. node modules
+"""
+def installNodeModules():
+	print "Installing node modules. This may take a while..."
+	rc = call(["npm", "install"])
+	validateReturnCode(rc)
 
 
 
 user = os.environ["USER"]
+
+installRTorrent()
+installScreen()
+
 installRtorrentDaemon(user, "./config/daemons/rtorrent")
 installPiTorrentDaemon(user, "./config/daemons/pitorrent")
 
+configRtorrent(user, "./config/rtorrent.rc")
+moveRtorrentConfig(user, "./config/rtorrent.rc")
+
+installNodeModules()
